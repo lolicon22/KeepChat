d71fced1349a67438af9650d0de80f08396b590a
P1nGu1n <p1ngu1n@outlook.com>
2014-12-25 23:00:28 +0100

    v4.1.0 release
    Reverted the force enable debugging used in the beta.

--- KeepChat/src/main/java/com/ramis/keepchat/KeepChat.java
+++ KeepChat/src/main/java/com/ramis/keepchat/KeepChat.java
@@ -423,8 +423,7 @@ public class KeepChat implements IXposedHookLoadPackage, IXposedHookZygoteInit {
             mSaveSentSnaps = sharedPreferences.getBoolean("pref_key_save_sent_snaps", mSaveSentSnaps);
             mSortByCategory = sharedPreferences.getBoolean("pref_key_sort_files_mode", mSortByCategory);
             mSortByUsername = sharedPreferences.getBoolean("pref_key_sort_files_username", mSortByUsername);
-            // mDebugging = sharedPreferences.getBoolean("pref_key_debug_mode", mDebugging);
-            mDebugging = true;
+            mDebugging = sharedPreferences.getBoolean("pref_key_debug_mode", mDebugging);
             Logger.setDebuggingEnabled(mDebugging);
 
             if (mDebugging) {
